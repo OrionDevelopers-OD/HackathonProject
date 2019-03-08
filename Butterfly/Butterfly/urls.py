cb224f262ae67c00d0710a9367382951adf84abd
Vivekasr <hie.vivi.sharma@gmail.com>
2019-03-08 16:46:58 +0530

    new app shopcart

--- Butterfly/Butterfly/urls.py
+++ Butterfly/Butterfly/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include('Butterfly_Home.urls'))
+    path('home/', include('Butterfly_Home.urls')),
+    path('cart/', include('shop.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
