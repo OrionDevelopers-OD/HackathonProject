c401cb615a4be7731e254d8187694be73e4c7533
Yashkrity <36617931+Yashkrity@users.noreply.github.com>
2019-03-08 18:04:07 +0530

    hfr

--- Butterfly/Butterfly/urls.py
+++ Butterfly/Butterfly/urls.py
@@ -5,11 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-<<<<<<< HEAD
     path('home/', include('Butterfly_Home.urls')),
     path('cart/', include('shop.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-=======
-    path('home/', include('Butterfly_Home.urls'))
 ]
->>>>>>> 37af6d780a515615af899f4fab7c343b59001662
