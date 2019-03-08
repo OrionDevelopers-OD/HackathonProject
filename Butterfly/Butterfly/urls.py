cdda334a70aeff0b40dd115e6f59aaef1c475399
Vivekasr <hie.vivi.sharma@gmail.com>
2019-03-08 18:01:18 +0530

    index html change

--- Butterfly/Butterfly/urls.py
+++ Butterfly/Butterfly/urls.py
@@ -6,5 +6,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', include('Butterfly_Home.urls')),
-    path('cart/', include('shop.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
