1afdc8d847c414497f9b0ee16e994cf1a18c4d44
Vivekasr <hie.vivi.sharma@gmail.com>
2019-03-08 18:13:14 +0530

    ..

--- Butterfly/Butterfly/urls.py
+++ Butterfly/Butterfly/urls.py
@@ -6,8 +6,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', include('Butterfly_Home.urls')),
-<<<<<<< HEAD
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-=======
-]
->>>>>>> 2233944291354dfca66f498fc4d9cb15ce520ab0
