57dcea254a40bccdb5ebb7ff819842102eff0133
Yashkrity <36617931+Yashkrity@users.noreply.github.com>
2019-03-08 16:14:54 +0530

    about

--- Butterfly/Butterfly/settings.py
+++ Butterfly/Butterfly/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'Butterfly',
     'Butterfly_Home',
+    'Butterfly_About',
 ]
 
 MIDDLEWARE = [
--- Butterfly/Butterfly/urls.py
+++ Butterfly/Butterfly/urls.py
@@ -1,19 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-<<<<<<< HEAD
-
 from django.conf import settings
-
-=======
 from django.conf import settings
->>>>>>> 51281c64d59bfbf8341664d359d836dd76df6ad3
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include('Butterfly_Home.urls'))
-<<<<<<< HEAD
+    path('home/', include('Butterfly_Home.urls')),
+    path('about/', include('Butterfly_About.urls')),
 ]
-=======
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
->>>>>>> 51281c64d59bfbf8341664d359d836dd76df6ad3
