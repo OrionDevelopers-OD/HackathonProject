16793c5f38c5b66d50b74d67ad5296df272da0c8
Yashkrity <36617931+Yashkrity@users.noreply.github.com>
2019-03-09 00:29:20 +0530

    links plants and home

--- Butterfly/Butterfly_Plants/views.py
+++ Butterfly/Butterfly_Plants/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def plants(request):
-    return render(request, "Plants/plants.html")
+    return render(request, "Butterfly_Plants/plants.html")
 
 # Create your views here.
