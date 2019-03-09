21a517a9ebc740691132608b08410b52015fe5a8
Yashkrity <36617931+Yashkrity@users.noreply.github.com>
2019-03-09 08:58:43 +0530

    fet

--- Butterfly/Butterfly/urls.py
+++ Butterfly/Butterfly/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admins.site.urls),
     path('home/', include('Butterfly_Home.urls')),
     path('plants/', include('Butterfly_Plants.urls')),
     path('seeds/', include('Butterfly_Seeds.urls',)),
